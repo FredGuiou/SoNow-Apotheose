7eb3018d4591265f9531b0194e767f6ab0d388e9
Sebastien Le Quellec <97674610+pKronik@users.noreply.github.com>
2022-07-18 17:20:00 +0200

    Adding structure models

---
+++ BackEnd/app/models/tag.js
@@ -0,0 +1,19 @@
+// const client = require("../config/db");
+
+module.exports = {
+  async findAll() {
+
+  },
+  async findByPk(tagId) {
+
+  },
+  async insert(tag) {
+
+  },
+  async update(id, tag) {
+
+  },
+  async delete(id) {
+    
+  }
+};

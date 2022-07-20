260f43d2b7a4cc1fe7adceb71c76e5363170c588
ElinorCm <elinor.compagnon@gmail.com>
2022-07-20 16:06:55 +0200

    Authentification component semantic UI functional architecture

--- FrontEnd/src/components/LoginForm.js
+++ FrontEnd/src/components/LoginForm.js
@@ -1,30 +1,14 @@
-import { Link } from "react-router-dom";
+import { Form } from 'semantic-ui-react';
 
 import '../styles/loginForm.scss';
 
 function LoginForm() {
-  return (
-    <div className="login">
-      <form className='login__form'>
-        <div className='login__form__input-container'>
-          <label>Email </label>
-          <input className='login__form__input' type='text' name='email' required />
-        </div>
-        <div className='login__form__input-container'>
-          <label>Mot de passe </label>
-          <input className='login__form__input' type='password' name='pass' required />
-        </div>
-        <div className='login__form__button'>
-          <input className='button--pink' type='submit' value="Connexion" />
-        </div>
-      </form>
-      <div className='login__authentification'>
-        <p className='login__authentification__text' >Vous n'avez pas de compte ?</p>
-        <div>
-          <Link className='login__authentification__link' to="/creer-un-compte">Inscrivez-vous</Link>
-        </div>    
-      </div>
-    </div>
+return (
+  <Form size='large' inverted>
+    <Form.Input fluid label='Email' placeholder='Email' />
+    <Form.Input fluid type='password' label='Mot de passe' placeholder='Mot de passe' />
+    <Form.Button>C'est parti !</Form.Button>
+  </Form>
 );
 
 }
--- FrontEnd/src/components/UserCard.js
+++ FrontEnd/src/components/UserCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/userCard.scss";
+import "../styles/usercard.scss";
 import { useParams } from "react-router";
 
 function UserCard() {

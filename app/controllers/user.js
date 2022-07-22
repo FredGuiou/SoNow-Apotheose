e14af1785145741efcee736d498ce8ea057bcf68
FredGuiou <frederic.guiou@gmail.com>
2022-07-22 17:17:00 +0200

    Update controller, router, datamapper / user, adding event controller, dataMapper

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,5 +1,4 @@
 //TODO: Gestion des erreurs via un controller error.
-//TODO: encyptage des mots de passes.
 //TODO: Implémentation de JOI validation schema.
 
 const userDataMapper = require('../models/user');
@@ -12,43 +11,63 @@ module.exports = {
         const reqEmail = req.body.email
         const reqPassword = req.body.password
 
-        //Vérifier auprès de la base donnée si elle connait un utilisateur 
         const userDb = await userDataMapper.findByEmail(reqEmail);
-        //Si la bdd ne connait pas l'utilisateur
         if(!userDb){
             //on renvoit une erreur
             // throw new ApiError('Login Error', {statusCode: 401});
             console.log("J'ai planté dans if !user");
-        }
+        };
         //On compare le mdp donnée par l'ulisateur avec celui de la bdd
         const checkPassword = await bcrypt.compare(reqPassword, userDb.password);
-        //Si le mot de passe ne correspond pas
         if (!checkPassword) {
             //alors je renvoie une erreur
             // throw new ApiError('Login Error', {statusCode: 401});
             console.log("J'ai planté dans le truc du machin chose password compare la j'sais plus quoi");
-        }
-        //On enregistre les données utilisateur en session req.session
+        };
         req.session.user = userDb
-        //On supprime le mot de passse par soucis de sécurité
         delete req.session.user.password;
-        //on revoit les données de l'utilisateur
         res.json(req.session.user); 
     },
     
+
+    //Méthode qui permet de récupérer tous les utilisateurs.
     async getAllUsers(_, res) {
-        
+        const userDb = await userDataMapper.findAll();
+        return res.json(userDb);
     },
 
-    async getOneUser(req, res) {
-        
+    //Méthode qui permet de récupérer un utilisateur par son Id.
+    async getOneUserById(req, res) {
+        const userDb = await userDataMapper.findByPk(req.params.id);
+        if(!userDb){
+            // throw new ApiError('This category does not exists', { statusCode: 404 });
+        };
+        return res.json(userDb);
     },
+
+    //Méthode qui permet de rechercher les utilisateurs par leur surnom, leur nom ou prénom.
+    async getOneUserByNameOrNickname(req, res) {
+
+        const userParams = {
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            nickname: req.body.nickname
+        };
+
+        const userDb = await userDataMapper.findByNameOrNickname(userParams);
+        if(!userDb){
+            // throw new ApiError('This category does not exists', { statusCode: 404 });
+        };
+        return res.json(userDb);
+    },
+
+
+
+
 
     //Méthode qui permet à l'utilisateur de se créer un compte sur l'application.
     async createUser(req, res) {
         const userDb = await userDataMapper.findByEmail(req.body.email);
-        //On vérifie si l'email n'est pas déjà pris.
-            //Si userDb n'existe pas
             if (!userDb) {
                 console.log(req.body);
                 //On crypte le mot de passe.
@@ -64,7 +83,7 @@ module.exports = {
                 res.json(insertUser);
             } else {
                 console.log("Ça bug ! :D");
-            }
+            };
             //On vérifie que le format de l'email soit valide avec Joi validation
                 //Si le format de la regex n'est pas respecté
                     //on envoie un message d'erreur
@@ -76,11 +95,21 @@ module.exports = {
     },
 
     async updateUser(req, res) {
-        
+        const userDb = await userDataMapper.findByPk(req.params.id);
+        if (!userDb) {
+            //throw new ApiError('This category does not exists', { statusCode: 404 });
+        };
+        const body = req.body;
+        const savedUser = await userDataMapper.update(body, req.params.id);
+        return res.json(savedUser);
     },
 
     async deleteUser(req, res) {
-        
-    },
-
+        const userDb = await userDataMapper.findByPk(req.params.id);
+        if (!userDb) {
+            // throw new ApiError('This category does not exists', { statusCode: 404 });
+        };
+        await userDataMapper.delete(req.params.id);
+        return res.status(204).json();
+    }
 };
--- app/models/user.js
+++ app/models/user.js
@@ -3,18 +3,63 @@
 const client = require("../config/db");
 
 module.exports = {
+
+  //Rechercher tous les utilisateurs de la BDD. 
   async findAll() {
+    try {
+      //Je prépare une requête sql séparément pour éviter les injections.
+      //J'utilise les jetons sql également par souci de sécurité.
+      const preparedQuery = {
+        text: `
+          SELECT *
+          FROM public.user
+        `
+      };
 
+      const result = await client.query(preparedQuery);
+
+      if (result.rowCount === 0) {
+        return undefined;
+    };
+
+    return result.rows[0];
+
+    } catch (error) {
+      console.log(error);
+      return null;
+    };
   },
-  async findByPk(userId) {
 
+  //Recherche un utilisateur en fonction de sa clé primaire ID.
+  async findByPk(userId) {
+    try {
+      //Je prépare une requête sql séparément pour éviter les injections.
+      //J'utilise les jetons sql également par souci de sécurité.
+      const preparedQuery = {
+        text: `
+          SELECT *
+          FROM public.user
+          WHERE id = $1
+        `,
+        values: [userId],
+      };
+      const result = await client.query(preparedQuery);
+      if (result.rowCount === 0) {
+        return undefined;
+    };
+
+    return result.rows[0];
+    } catch (error) {
+      console.log(error);
+      return null;
+    };
   },
 
   //Retrouver un user par son email pour l'authentification
   async findByEmail(reqEmail){
     try {
       //Je prépare une requête sql séparément pour éviter les injections.
-      //J'utilise les jetons $1, et l'agument [email] également par souci de sécurité.
+      //J'utilise les jetons sql également par souci de sécurité.
       const preparedQuery = {
         text: `
           SELECT *
@@ -23,36 +68,123 @@ module.exports = {
         `,
         values: [reqEmail],
       };
-      //J'envoie ma requête auprès de la DB.
+
       const result = await client.query(preparedQuery);
+
       if (result.rowCount === 0) {
         return undefined;
-    }
+    };
 
     return result.rows[0];
-    } catch (sqlError) {
-      console.log(sqlError);
+
+    } catch (error) {
+      console.log(error);
+      return null;
+    };
+  },
+
+  //Rechercher un utilisateur par son surnom, son nom ou son prénom.
+  async findByNameOrNickname(nickName, firstName, lastName){
+    try {
+      //Je prépare une requête sql séparément pour éviter les injections.
+      //J'utilise les jetons sql également par souci de sécurité.
+      const preparedQuery = {
+        text: `
+          SELECT *
+          FROM public.user
+          WHERE nickname = $1
+          OR firstname = $2
+          OR lastname = $3
+          OR id = $4
+        `,
+        values: [nickName, firstName, lastName, id],
+      };
+
+      const result = await client.query(preparedQuery);
+
+      if (result.rowCount === 0) {
+        return undefined;
+    };
+
+    return result.rows[0];
+
+    } catch (error) {
+      console.log(error);
+      return null;
+    };
+  },
+
+  //Insérer un nouvel utilisateur dans la BDD.
+  async insert(newUser) {
+    try {
+      //Je prépare une requête sql séparément pour éviter les injections.
+      //J'utilise les jetons sql également par souci de sécurité.
+      const preparedQuery = {
+        text: `
+        INSERT INTO public.user
+        (firstname, lastname, nickname, email, password) VALUES
+        ($1, $2, $3, $4, $5) RETURNING *
+        `,
+        values: [newUser.firstname, newUser.lastname, newUser.nickname, newUser.email, newUser.password],
+      };
+  
+      const result = await client.query(preparedQuery);
+  
+    return result.rows[0];
+
+    } catch (error) {
+      console.log(error);
       return null;
     }
   },
 
-  async insert(newUser) {
-    const savedUser = await client.query(
-      `
-          INSERT INTO public.user
-          (firstname, lastname, nickname, email, password) VALUES
-          ($1, $2, $3, $4, $5) RETURNING *
-      `,
-      [newUser.firstname, newUser.lastname, newUser.nickname, newUser.email, newUser.password],
-  );
-
-  return savedUser.rows[0];
-  },
-
+  //Mettre à jours les infos d'un utilisateur en BDD.
   async update(id, user) {
+    try {
+      //On récupère les champs et les valeurs de l'utilisateur
+      const fields = Object.keys(user).map((prop, index) => `"${prop}" = $${index + 1}`);
+      const values = Object.values(user);
+      //Je prépare une requête sql séparément pour éviter les injections.
+      //J'utilise les jetons sql également par souci de sécurité.
+      const preparedQuery = {
+        text: `
+        UPDATE public.user SET
+        ${fields}
+        WHERE id = $${fields.length + 1}
+        RETURNING *        
+        `,
+        values: [...values, id],
+      };
+  
+      const result = await client.query(preparedQuery);
+  
+    return result.rows[0];
 
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
   },
+  
+  //Supprimer un utilisateur de la BDD.
   async delete(id) {
+    try {
+      const preparedQuery = {
+        text: `
+          DELETE
+          FROM public.user
+          WHERE id = $1
+        `,
+        values: [id],
+      };
 
+      const result = await client.query(preparedQuery);
+
+      return !!result.rowCount;
+      
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
   }
 };

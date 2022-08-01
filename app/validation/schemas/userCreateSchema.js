c38c4d22e622c19ce72cfdebd13db5a7817291a4
FredGuiou <frederic.guiou@gmail.com>
2022-08-01 15:26:45 +0200

    Finish validation for user on Create and Update method

--- app/validation/schemas/userCreateSchema.js
+++ app/validation/schemas/userCreateSchema.js
@@ -1,14 +1,9 @@
 const Joi = require('joi');
 
 module.exports = Joi.object({
-    label: Joi.string().required(),
-    route: Joi.string()
-        .pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)  // On place une regex validant le format d'un email (caractère alphanumérique & .-_  @  caractères alpha . 2 à 4 caractères alpha)
-        .required(),
-},
-{
-    label: Joi.string().required(),
-    route: Joi.string()
-        .pattern(/^(?=.*[0-9])(?=.*[az])(?=.*[AZ])(?=.*[@#$%^&-+=() ])(?=\\S+$).{8, 20}$/)  // On place une regex gérants les contraintes suivantes 8 caract min 20 max, avec au moins 1 chiffre une minuscule une majuscule, et un carctère spéciale incluant  !@#$%&*()-+=^ on exclut les "espace"
-        .required(),
+    email: Joi.string().required().regex(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/),
+    password: Joi.string().required().regex(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[-+!*$@%_])([-+!*$@%_\w]{8,15})$/),
+    firstname: Joi.string().required(),
+    lastname: Joi.string().required(),
+    nickname: Joi.string().required(),
 }).required();

baca0d01d5097ae86eb62d40bf3218d37cedf162
Romain P <portanguenr@gmail.com>
2022-08-03 12:01:38 +0200

    refactoring of the authentication page

--- FrontEnd/src/components/Authentification.js
+++ FrontEnd/src/components/Authentification.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Container, Grid, Header } from 'semantic-ui-react';
+import { Header } from 'semantic-ui-react';
  
 import '../styles/authentification.scss';
 import Login from './AuthentificationLogin';
@@ -16,20 +16,18 @@ return (
     </div>
     <div className='authentification__main'>
         <img className='authentification__main__logo' src={logo} alt='SoNow application logo' />
-        <Container className="login__grid-container" style={{display: 'flex', justifyContent: 'center'}}>
-          <Grid className="login__grid-content" style={{display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
-            <Grid.Column className="login__form-container" style={{margin: '1em 0 1em 0', padding: '0.5em 0 0.5em 0'}}>
+        <div className="login-container" style={{width: '25rem', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+            <div className="login-container__form" style={{}}>
               <Header inverted as='h1' textAlign='center' style={{marginTop: '15px'}}>
                 Se connecter
               </Header>
               <Login />
-              <div className="login__form-footer">
+              <div className="login-container__form__footer">
               <p>Vous n'avez pas encore de compte ?</p>
-              <Link className='create-account__link' to="/creer-un-compte">Inscrivez-vous</Link>
+              <Link className='login-container__form__footer__link' to="/creer-un-compte">Inscrivez-vous</Link>
               </div>
-            </Grid.Column>
-          </Grid>
-        </Container>
+            </div>
+        </div>
     </div>
   </div>
 );
